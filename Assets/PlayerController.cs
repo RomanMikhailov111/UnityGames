894c7206247cc697b827f53808000cf32f98aed8
RomanMikhailov <chap9iduk@gmail.com>
2023-09-30 15:44:51 +0300

    ADD Horizontal

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed;
+    public float horizontalInput;
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = new Vector3(5, 6, 9); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
+        Vector3 dir = Vector3.forward;
+        transform.Translate(dir * Time.deltaTime * speed * horizontalInput);
     }
 }
