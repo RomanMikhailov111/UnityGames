37a92deaf21d3ada8a72e2852f9844ffa49f1514
RomanMikhailov <chap9iduk@gmail.com>
2023-10-07 15:36:17 +0300

    AddVertical

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,17 +6,20 @@ public class PlayerController : MonoBehaviour
 {
     public float speed;
     public float horizontalInput;
+    public float verticalInput;
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(5, 6, 9); 
+       //  transform.position = new Vector3(3, 3, 23); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        Vector3 dir = Vector3.forward;
-        transform.Translate(dir * Time.deltaTime * speed * horizontalInput);
+        horizontalInput = Input.GetAxis("Horizontal") ;
+        verticalInput = Input.GetAxis("Vertical") ;
+      
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * horizontalInput);
+        transform.Translate(Vector3.right * Time.deltaTime * speed * verticalInput);
     }
 }
