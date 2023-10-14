116e32db4e28a90f030fc5ba24b1e63382580ae8
RomanMikhailov <chap9iduk@gmail.com>
2023-10-14 15:40:49 +0300

    ADDMultiplayer

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Windows;
 
 public class PlayerController : MonoBehaviour
 {
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode keyCode;
     public float speed;
     public float rotateSpeed;
     public float horizontalInput;
     public float verticalInput;
+    public string inputID = null;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,10 +22,17 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal") ;
-        verticalInput = Input.GetAxis("Vertical") ;
+        horizontalInput = Input.GetAxis("Horizontal"+ inputID) ;
+        verticalInput = Input.GetAxis("Vertical" + inputID) ;
       
         transform.Translate(Vector3.forward * Time.deltaTime * speed * horizontalInput);
         transform.Rotate(Vector3.up, Time.deltaTime * rotateSpeed * verticalInput);
+
+        if (Input.GetKeyDown(keyCode)) 
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+            
+        }
     }
 }
