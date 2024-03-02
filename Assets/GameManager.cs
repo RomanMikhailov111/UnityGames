b2ce6746ce5891cdcab3d28e37123623e9987d08
RomanMikhailov <chap9iduk@gmail.com>
2024-03-02 13:01:34 +0200

    ADDGamePanel

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -6,25 +6,77 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+public enum GameType
+{
+    Easy,
+    Medium,
+    Hard
+}
 public class GameManager : MonoBehaviour
 {
-    private float SpawnRate = 1.0f;
+    public float EasySpawnRate = 1.5f;
+    public float MediumSpawnRate = 1.0f;
+    public float HardSpawnRate = 0.5f;
     public int Score;
     public bool isGameOver;
     public GameObject[] TargetsPrefabs;
 
     public Button RestartButton;
+    public Button EasyStartButton;
+    public Button MediumStartButton;
+    public Button HardStartButton;
 
 
     public GameObject GameOverPanel;
+    public GameObject StartGamePanel;
 
     public TextMeshProUGUI ScoreText;
     void Start()
     {
         RestartButton.onClick.AddListener(RestartGame);
 
+        EasyStartButton.onClick.AddListener(EasyStartGame);
+        MediumStartButton.onClick.AddListener(MediumStartGame);
+        HardStartButton.onClick.AddListener(HardStartGame);
+
         UpdateScore(0);
-        StartCoroutine(SpawnTarget());
+    }   
+
+    private void StartGame(GameType type)
+    {
+        float TimeToSpawn = 0;
+
+        switch (type)
+        {
+            case GameType.Easy:
+                TimeToSpawn = EasySpawnRate;
+                break;
+            case GameType.Medium:
+                TimeToSpawn = MediumSpawnRate;
+                break;
+            case GameType.Hard:
+                TimeToSpawn = HardSpawnRate;
+                break;
+        }
+
+        StartCoroutine(SpawnTarget(TimeToSpawn));
+        StartGamePanel.SetActive(false);
+    }
+    private void HardStartGame()
+    {
+
+        StartGame(GameType.Hard);
+    }
+
+    private void MediumStartGame()
+    {
+        StartGame(GameType.Medium);
+    }
+
+    private void EasyStartGame()
+    {
+
+        StartGame(GameType.Easy);
     }
 
     private void RestartGame()
@@ -46,11 +98,11 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    private IEnumerator SpawnTarget ()
+    private IEnumerator SpawnTarget (float TimeSpawnRate)
     {
         while (!isGameOver)
         {
-            yield return new WaitForSeconds(SpawnRate);
+            yield return new WaitForSeconds(TimeSpawnRate);
             int Index = Random.Range(0, TargetsPrefabs.Length);
             Instantiate(TargetsPrefabs[Index]);
         }
